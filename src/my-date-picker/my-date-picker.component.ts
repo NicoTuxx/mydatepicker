f79f41beaefdb56afccd68b9ab68eb9a45e6fb9b
Nicolas Stempert <nicolasstempert@gmail.com>
2016-10-26 16:31:06 +0200

    add keyup events on calendar

--- src/my-date-picker/my-date-picker.component.ts
+++ src/my-date-picker/my-date-picker.component.ts
@@ -191,8 +191,6 @@ export class MyDatePicker implements OnChanges {
     }
 
     userCalendar(event:any):void {
-      event.stopPropagation();
-      event.preventDefault();
       let d = (this.selectedDate.day === 0) ? this.today.getDate() : this.selectedDate.day;
       let m = (this.selectedDate.month === 0) ? this.today.getMonth() : this.selectedDate.month - 1;
       let y = (this.selectedDate.year === 0) ? this.today.getFullYear() : this.selectedDate.year;
