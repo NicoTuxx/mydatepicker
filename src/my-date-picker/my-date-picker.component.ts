70982409fa7487767934aef4ac160ee882005fdb
Nicolas Stempert <nicolasstempert@gmail.com>
2016-11-10 16:30:25 +0100

    add label options for clear and picker buttons

--- src/my-date-picker/my-date-picker.component.ts
+++ src/my-date-picker/my-date-picker.component.ts
@@ -65,6 +65,8 @@ export class MyDatePicker implements OnChanges {
     editableMonthAndYear: boolean = true;
     minYear: number = 1000;
     maxYear: number = 9999;
+    pickerBtnTxt: string = 'Select a date';
+    clearBtnTxt: string = 'Clear the date';
     required: boolean = false;
     title: string = 'Choose a date';
     accessibility: boolean = false;
@@ -96,7 +98,7 @@ export class MyDatePicker implements OnChanges {
     }
 
     setOptions():void {
-        let options = ['dayLabels', 'monthLabels', 'dateFormat', 'todayBtnTxt', 'firstDayOfWeek', 'sunHighlight', 'disableUntil', 'disableSince', 'disableWeekends', 'height', 'width', 'selectionTxtFontSize', 'inline', 'alignSelectorRight', 'indicateInvalidDate', 'showDateFormatPlaceholder', 'editableMonthAndYear', 'minYear', 'maxYear', 'required', 'title', 'accessibility'];
+        let options = ['dayLabels', 'monthLabels', 'dateFormat', 'todayBtnTxt', 'firstDayOfWeek', 'sunHighlight', 'disableUntil', 'disableSince', 'disableWeekends', 'height', 'width', 'selectionTxtFontSize', 'inline', 'alignSelectorRight', 'indicateInvalidDate', 'showDateFormatPlaceholder', 'editableMonthAndYear', 'minYear', 'maxYear', 'pickerBtnTxt', 'clearBtnTxt', 'required', 'title', 'accessibility'];
         for (let prop of options) {
             if (this.options && (this.options)[prop] !== undefined  && (this.options)[prop] instanceof Object) {
                 (this)[prop] = JSON.parse(JSON.stringify((this.options)[prop]));
