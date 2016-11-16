09009d5a73881682bb57679b04c44dcabfd73c08
Nicolas Stempert <nicolasstempert@gmail.com>
2016-11-16 15:08:54 +0100

    add invisible month and year span for screenreaders

--- src/my-date-picker/pipes/my-date.pipe.ts
+++ src/my-date-picker/pipes/my-date.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'my-date'
+  name: 'myDate'
 })
 export class MyDatePipe implements PipeTransform {
   transform(date: {year: number, month: number, day: number}, type: string): number {
