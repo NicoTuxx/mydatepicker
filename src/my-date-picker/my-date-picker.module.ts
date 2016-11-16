61072ffb675609daac5278104fb96057825c4c94
Nicolas Stempert <nicolasstempert@gmail.com>
2016-11-16 14:09:55 +0100

    add invisible month and year span for screenreaders

--- src/my-date-picker/my-date-picker.module.ts
+++ src/my-date-picker/my-date-picker.module.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {MyDatePicker} from './my-date-picker.component';
 import {InputFocusDirective} from './directives/my-date-picker.input.directive';
-import {MyDatePipe} from './pipes/my-date.pipe';
+import {MyDatePipe} from './pipes';
 
 @NgModule({
     imports: [ CommonModule ],
