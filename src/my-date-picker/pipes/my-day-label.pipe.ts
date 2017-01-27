11d79601834ace7783902e16cca6c48a63ef2af1
Nicolas Stempert <nicolasstempert@gmail.com>
2017-01-27 12:56:52 +0100

    feat: internationalize the datepicker

--- src/my-date-picker/pipes/my-day-label.pipe.ts
+++ src/my-date-picker/pipes/my-day-label.pipe.ts
@@ -1,18 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {LocaleService} from './services/my-date-picker.locale.service';
 import {LongLabelService} from '../services/my-date-picker.long-label.service';
 
 @Pipe({
   name: 'myDayLabel'
 })
 export class MyDayLabelPipe implements PipeTransform {
-  constructor(private longLabelService: LongLabelService) {
+  constructor(
+    private longLabelService: LongLabelService,
+    private localeService: LocaleService) {
 
   }
 
   transform(day: string, locale: string): string {
-    console.log(locale);
-    let longLabels = this.longLabelService.getLongLabels('fr');
+    let lang = (locale !== undefined) ? locale : 'en';
+    let longLabels = this.longLabelService.getLongLabels(lang);
+    let options = this.localeService.getLocaleOptions(lang);
     console.log(longLabels);
+    console.log(options);
 
     switch (day) {
       case 'Lun':
